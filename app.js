73e7d6b7c6c753a849d4af4f5f49e336580d45b0
roycevanle <roycele123@gmail.com>
2021-09-14 01:10:00 -0700

    made personal notes & added startGame, generatingObstacles, jump with space, and css

--- app.js
+++ app.js
@@ -11,12 +11,58 @@ document.addEventListener('DOMContentLoaded' , () => {
     let gravity = 2
 
     function startGame() {
+        birdBottom -= gravity
         //adds a 100px from the bottom element
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
 
-    startGame()
+    // we invoke startGame every 20ms
+    let timerId = setInterval(startGame, 20)
+    // if we want to stop, we do clearInterval(timerId)
+
+    function control(e) {
+        if(e.keyCode === 32) { //keyCode 32 is spacebar
+            jump()
+        }
+        // DEBUGGING feature I added to stop bird
+        if(e.keyCode === 38) {
+            clearInterval(timerId)
+        }
+    }
+
+    function generateObstacle() {
+        let obstacleLeft = 500
+        // will generate div height of 1-60 from the ground
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
+        // how to create divs (which we'll use as obstacles)
+        // add the class of obstacle to this obstacle object (to this custom div)
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+        }
+
+        //move obstacle to the left every 20ms
+        let timerId = setInterval(moveObstacle, 20)
+    }
+    generateObstacle();
+
+    function jump() {
+        // add an upper echelon so that we don't jump off the screen
+        if (birdBottom < 500) birdBottom += 50
+        bird.style.bottom = bird + 'px'
+        console.log(birdBottom)
+    }
+
+    // keyup event is fired when a key is released (not when up arrow pressed)
+    document.addEventListener('keyup', control);
 
 
 })
