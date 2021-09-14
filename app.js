d41d3f94c1dd403bcad30cc49446764b1b2f5ce4
roycevanle <roycele123@gmail.com>
2021-09-14 15:31:58 -0700

    added logic for gameOver

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     let birdLeft = 220
     let birdBottom = 100
     let gravity = 2
+    let isGameOver = false
 
     function startGame() {
         birdBottom -= gravity
@@ -18,49 +19,14 @@ document.addEventListener('DOMContentLoaded' , () => {
     }
 
     // we invoke startGame every 20ms
-    let timerId = setInterval(startGame, 20)
-    // if we want to stop, we do clearInterval(timerId)
+    let gameTimerId = setInterval(startGame, 20)
+    // if we want to stop, we do clearInterval(gameTimerId)
 
     function control(e) {
         if(e.keyCode === 32) { //keyCode 32 is spacebar
             jump()
         }
-        // DEBUGGING feature I added to stop bird
-        if(e.keyCode === 38) {
-            clearInterval(timerId)
-        }
     }
-
-    function generateObstacle() {
-        let obstacleLeft = 500
-        // will generate div height of 1-60 from the ground
-        let randomHeight = Math.random() * 60
-        let obstacleBottom = randomHeight
-        // how to create divs (which we'll use as obstacles)
-        // add the class of obstacle to this obstacle object (to this custom div)
-        const obstacle = document.createElement('div')
-        obstacle.classList.add('obstacle')
-        gameDisplay.appendChild(obstacle)
-        obstacle.style.left = obstacleLeft + 'px'
-        obstacle.style.bottom = obstacleBottom + 'px'
-
-        function moveObstacle() {
-            obstacleLeft -= 2
-            obstacle.style.left = obstacleLeft + 'px'
-            
-            if (obstacleLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstacle)
-            }
-        }
-
-        // setInterval is generally used for animation (less delay) than setTimeout
-        // move obstacle to the left every 20ms
-        let timerId = setInterval(moveObstacle, 20)
-        // setTimeout calls a function after a specified number of ms
-        setTimeout(generateObstacle, 3000)
-    }
-    generateObstacle();
 
     function jump() {
         // add an upper echelon so that we don't jump off the screen
@@ -71,6 +37,58 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     // keyup event is fired when a key is released (not when up arrow pressed)
     document.addEventListener('keyup', control);
+
+    function generateObstacle() {
+        let obstacleLeft = 500
+        // will generate div height of 1-60 from the ground
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
+        // how to create divs (which we'll use as obstacles)
+        // add the class of obstacle to this obstacle object (to this custom div)
+        const obstacle = document.createElement('div')
+        if (!isGameOver) obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+            
+            // if it reaches left edge, delete the obstacle
+            if (obstacleLeft === -60) {
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+
+            // if bird reached the floor || hits an obstacle, gamveOver
+            if (
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+                birdBottom === 0
+                ) {
+                gameOver()
+                clearInterval(timerId)
+            }
+        }
+
+        // setInterval is generally used for animation (less delay) than setTimeout
+        // move obstacle to the left every 20ms
+        let timerId = setInterval(moveObstacle, 20)
+        // setTimeout calls a function after a specified number of ms
+        // generates obstacle only if not gameOver
+        if (!isGameOver) setTimeout(generateObstacle, 3000)
+    }
+
+
+    generateObstacle();
+
+    function gameOver() {
+        clearInterval(gameTimerId)
+        console.log('game over')
+        isGameOver = true
+        document.removeEventListener('keyup', control)
+    }
+
 
 
 })
