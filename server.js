936df9c96937ff13e7eb0feb560a2aed4c27c08a
roycevanle <roycele123@gmail.com>
2021-09-15 00:42:13 -0700

    added logic for client Ids/tokens

--- server.js
+++ server.js
@@ -9,12 +9,36 @@ const Ably = require("ably");
 const app = express();
 app.use(express.static("public"));
 
+const realtime = new Ably.Realtime({
+    key: process.env.ABLY_API_KEY,
+});
+
 // if the app gets a request to the rootfolder, serve the html
 app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html");
-})
+});
+
+const uniqueId = function() {
+    return "id-" + Math.random().toString(36).substr(2,16);
+}
+
+/* Issue token requests to clients sending a request to the /auth endpoint */
+app.get('/auth', function (req, res) {
+    var tokenParams = {
+        clientId: 
+    };
+    //replace rest. with realtime.
+    realtime.auth.createTokenRequest(tokenParams, function(err, tokenRequest) {
+      if (err) {
+        res.status(500).send('Error requesting token: ' + JSON.stringify(err));
+      } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(tokenRequest));
+      }
+    });
+});
 
 // express server listens on a particular port
 const listener = app.listen(process.env.PORT, () => {
     console.log("App is listening on port " + listener.address().port);
-})
+});
